d4604b9259bd5cf09f07ba39cb9409993d10c520
Max Stevens <maxstevens2708@gmail.com>
2023-09-06 16:48:16 +0200

    Don't do let's encrypt config

--- .github/scripts/init/executeInstaller.sh
+++ .github/scripts/init/executeInstaller.sh
@@ -12,8 +12,8 @@ cd ~/installer
 sed -e "s/remote-server-ip-address/$HOST/" -e "s/root/$ANSIBLE_USER/" hosts.example > hosts
 
 # Configuration for Letsencrypt
-cd ~/installer/group_vars
-sed -i -e 's/#domain:\ your_domain.com/domain:\ $DOMAIN/g' -e 's/Madrid/Amsterdam/g' -e 's/#letsencrypt_email: "your_email@example.com"/letsencrypt_email: "$CONTACT_EMAIL"/g' ./all
+# cd ~/installer/group_vars
+# sed -i -e 's/#domain:\ your_domain.com/domain:\ $DOMAIN/g' -e 's/Madrid/Amsterdam/g' -e 's/#letsencrypt_email: "your_email@example.com"/letsencrypt_email: "$CONTACT_EMAIL"/g' ./all
 
 # Run the ansible playbook
 cd ~/installer
