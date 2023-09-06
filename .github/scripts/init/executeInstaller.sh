1eb284d600280bd9f6479c9780830ec8ec94be1f
Max Stevens <maxstevens2708@gmail.com>
2023-09-06 14:22:42 +0200

    Run full installer script

--- .github/scripts/init/executeInstaller.sh
+++ .github/scripts/init/executeInstaller.sh
@@ -6,15 +6,15 @@
 
 # Checkout installer
 git clone $INSTALLER_REPO ~/installer
-# cd ~/installer
-#
-# # Create a hosts file
-# sed -e "s/remote-server-ip-address/$HOST/" -e "s/root/$ANSIBLE_USER/" hosts.example > hosts
-#
-# # Configuration for Letsencrypt
-# cd ~/installer/group_vars
-# sed -i -e 's/#domain:\ your_domain.com/domain:\ $DOMAIN/g' -e 's/Madrid/Amsterdam/g' -e 's/#letsencrypt_email: "your_email@example.com"/letsencrypt_email: "$CONTACT_EMAIL"/g' ./all
-#
-# # Run the ansible playbook
-# cd ~/installer
-# ansible-playbook -v consul.yml -i hosts
+cd ~/installer
+
+# Create a hosts file
+sed -e "s/remote-server-ip-address/$HOST/" -e "s/root/$ANSIBLE_USER/" hosts.example > hosts
+
+# Configuration for Letsencrypt
+cd ~/installer/group_vars
+sed -i -e 's/#domain:\ your_domain.com/domain:\ $DOMAIN/g' -e 's/Madrid/Amsterdam/g' -e 's/#letsencrypt_email: "your_email@example.com"/letsencrypt_email: "$CONTACT_EMAIL"/g' ./all
+
+# Run the ansible playbook
+cd ~/installer
+ansible-playbook -v consul.yml -i hosts
