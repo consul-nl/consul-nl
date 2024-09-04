f8f216eb77976e6c3d64ec980dea33ed0d13c388
Max Stevens <maxstevens2708@gmail.com>
2024-09-04 16:43:11 +0200

    remove broken custom app code

--- config/initializers/application_custom.rb
+++ config/initializers/application_custom.rb
@@ -5,14 +5,6 @@ module Consul
       config.i18n.available_locales = ["nl"]
       config.i18n.enforce_available_locales = false
       config.i18n.fallbacks = { "nl" => "en" }
-
-      config.to_prepare do
-        previous_secret = Rails.application.secrets.secret_key_base_old
-
-        if previous_secret.present?
-          ActiveStorage.verifier.rotate(previous_secret)
-        end
-      end
     end
   end
 end
