69468928928267463c843d64e6f11164abae4cdb
Max Stevens <maxstevens2708@gmail.com>
2024-12-31 14:54:11 +0100

    apply oldenzaal tenant

--- app/components/custom/layout/subnavigation_component.rb
+++ app/components/custom/layout/subnavigation_component.rb
@@ -1,6 +1,11 @@
 class Layout::SubnavigationComponent < ApplicationComponent; end
 
-load Rails.root.join("app", "components", "layout", "subnavigation_component.rb")
+require_dependency Rails.root.join("app", "components", "layout", "subnavigation_component").to_s
 
-class Admin::TableActionsComponent
+class Layout::SubnavigationComponent < ApplicationComponent
+  delegate :current_user, to: :helpers
+
+  def budgets
+    Budget.open_budgets_for(current_user)
+  end
 end
