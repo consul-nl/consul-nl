014db0786be1ea34bc361c44c72200be75e1dca1
Max Stevens <maxstevens2708@gmail.com>
2025-04-01 14:37:13 +0200

    update fryslan footer

--- app/components/custom/layout/footer_component.rb
+++ app/components/custom/layout/footer_component.rb
@@ -1,3 +1,5 @@
-class Layout::FooterComponent < ApplicationComponent; end
-
 load Rails.root.join("app", "components", "layout", "footer_component.rb")
+
+class Layout::FooterComponent
+  delegate :image_path_for, to: :helpers
+end
--- tenants/fryslan/app/components/custom/layout/footer_component.rb
+++
@@ -1,3 +0,0 @@
-class Layout::FooterComponent < ApplicationComponent; end
-
-load Rails.root.join("app", "components", "layout", "footer_component.rb")
